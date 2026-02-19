30a086d323673818a0274ab97ced2c65be0f2041
DonjorTech <donjortech.corporation@hotmail.com>
2026-02-19 10:03:24 -0500

    Back update 1.1

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 const mysql = require("mysql2");
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -6,7 +7,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     waitForConnections: true,
-    connectionLimit: 100,
+    connectionLimit: 15,
     queueLimit: 0
 });
 
--- back/src/server.js
+++ back/src/server.js
@@ -1,2 +1,6 @@
-const http = require('http');
-const hostname = require('hostname')
+require("dotenv").config();
+const app = require("./src/app");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log("Servidor corriendo ");
+});
